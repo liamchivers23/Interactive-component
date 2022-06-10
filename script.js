e9c226d1397deb1cd3e140c15ae6cc9458d6e7f6
Liam Chivers <85449720+liamchivers23@users.noreply.github.com>
2022-06-10 20:52:03 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const ratings = document.querySelectorAll(".list-item");
+const submit = document.querySelector(".submit-btn");
+const rating = document.querySelector(".rating");
+const container = document.querySelector(".container");
+const thankYouState = document.querySelector(".thankyou-state");
+
+ratings.forEach((e) => {
+  e.addEventListener("click", () => {
+    rating.textContent = `${e.textContent}`;
+  });
+});
+
+submit.addEventListener("click", () => {
+  container.classList.toggle("hide");
+  thankYouState.classList.toggle("hide");
+});
